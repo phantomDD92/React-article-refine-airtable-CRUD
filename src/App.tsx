1618fc6b7d74c508e1d5ef37b46d61dcfd3258d7
lovelymonkey0612 <lovelymonkey0612@gmail.com>
2024-04-07 10:45:48 -0400

    Initial commit from create-refine-app

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import dataProvider from "@refinedev/airtable";
+import routerBindings, {
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router-v6";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  const API_TOKEN =
+    "patI3quNRP17TNsjK.d59600d5955939ed02110fb1107036ff4482496004f020f5bf031f55789cd321";
+  const BASE_ID = "appKYl1H4k9g73sBT";
+
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <DevtoolsProvider>
+          <Refine
+            dataProvider={dataProvider(API_TOKEN, BASE_ID)}
+            routerProvider={routerBindings}
+            options={{
+              syncWithLocation: true,
+              warnWhenUnsavedChanges: true,
+              useNewQueryKeys: true,
+              projectId: "mUwgD8-WDCMG7-bQOQuy",
+            }}
+          >
+            <Routes>
+              <Route index element={<WelcomePage />} />
+            </Routes>
+            <RefineKbar />
+            <UnsavedChangesNotifier />
+            <DocumentTitleHandler />
+          </Refine>
+          <DevtoolsPanel />
+        </DevtoolsProvider>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
